a86175a88640ca74ab6ae85bcde5029efa6c23d6
Lukas Gustavsson <lukas.gustavsson@hotmail.com>
2019-05-08 14:25:51 +0200

    början på den optimala linsen :)

--- src/optics_object_factories/OptimalConvexLensFactory.java
+++ src/optics_object_factories/OptimalConvexLensFactory.java
@@ -1,5 +1,8 @@
 package optics_object_factories;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import optics_objects.materials.ConvexLens;
 import optics_objects.materials.OptimalConvexLens;
 import optics_objects.templates.OpticsObject;
 import util.Vector2d;
@@ -11,6 +14,8 @@ public class OptimalConvexLensFactory extends OpticsObjectFactory {
 		addSlider("Radius 1", 55, 800, 300);
 		addSlider("Radius 2", 55, 800, 300);
 		addSlider("Refractionindex", 1, 3, 1.5);
+
+		addCheckBox("Show optical axis");
 	}
 
 	@Override
@@ -18,18 +23,50 @@ public class OptimalConvexLensFactory extends OpticsObjectFactory {
 		double d = getParam("Diameter");
 		double r1 = getParam("Radius 1");
 		double r2 = getParam("Radius 2");
-		if(r1*2 >= d && r2*2 >= d) {
-			return new OptimalConvexLens(origin, d, r1, r2,
-					getParam("Refractionindex"), fixedPos());
+		if (r1 * 2 >= d && r2 * 2 >= d) {
+			return new ConvexLens(origin, d, r1, r2, getParam("Refractionindex"), fixedPos(),
+					getBoxParam("Show optical axis"));
 		} else {
 			return null;
 		}
 	}
 
+	private boolean correctParams() {
+		return getParam("Radius 1") * 2 >= getParam("Diameter") && getParam("Radius 2") * 2 >= getParam("Diameter");
+	}
+
 	@Override
 	public void updateOpticsObject(OpticsObject object) {
-		// TODO Auto-generated method stub
-		
+		double d = getParam("Diameter");
+		double r1 = getParam("Radius 1");
+		double r2 = getParam("Radius 2");
+		if (r1 * 2 >= d && r2 * 2 >= d) {
+			((ConvexLens) object).setPoints(d, r1, r2);
+		}
+
+		((ConvexLens) object).setRefractionIndex(getParam("Refractionindex"));
+		((ConvexLens) object).showOpticalAxis(getBoxParam("Show optical axis"));
+	}
+
+	@Override
+	public void setListeners(EventHandler e) {
+		super.setListeners(e);
+		String[] relSliders = { "Radius 1", "Radius 2", "Diameter" };
+
+		EventHandler onIncorrectParams = event -> {
+			if (!correctParams()) {
+				for (String s : relSliders)
+					sliders.get(s).setStyle("-fx-background-color:red");
+			} else {
+				for (String s : relSliders)
+					sliders.get(s).setStyle("");
+			}
+		};
+
+		for (String s : relSliders) {
+			sliders.get(s).addEventHandler(MouseEvent.MOUSE_CLICKED, onIncorrectParams);
+			sliders.get(s).addEventFilter(MouseEvent.MOUSE_DRAGGED, onIncorrectParams);
+		}
 	}
 
 }
--- src/optics_objects/materials/OptimalConvexLens.java
+++ src/optics_objects/materials/OptimalConvexLens.java
@@ -1,16 +1,47 @@
 package optics_objects.materials;
 
+import javafx.scene.canvas.GraphicsContext;
+import optics_logic.OpticsSettings;
 import optics_objects.templates.Lens;
 import util.Vector2d;
 
 public class OptimalConvexLens extends Lens {
 	private static final long serialVersionUID = 1L;
 
-	public OptimalConvexLens(Vector2d origin, double d, double r1, double r2, double refractionindex, boolean fixedPosition) {
-		super(origin, refractionindex, fixedPosition); //Ta inte bort
-		
-		int quarterResolution = getResolution() /4;
-		
+	private double r1;
+	private double r2;
+
+	public OptimalConvexLens(Vector2d origin, double d, double r1, double r2, double refractionindex,
+			boolean fixedPosition, boolean showAxis) {
+		super(origin, refractionindex, fixedPosition);
+
+		showOpticalAxis(showAxis);
+		setPoints(d, r1, r2);
+
+		this.r1 = r1;
+		this.r2 = r2;
+
+	}
+
+	@Override
+	public void draw(GraphicsContext gc, OpticsSettings settings) {
+		super.draw(gc, settings);
+
+		if (showOpticalAxis) {
+			double f = 1 / ((refractionindex - 1) * (1 / r1 + 1 / r2));
+
+			Vector2d fPointR = getOrigin().copy().add(new Vector2d(f, 0).rotate(totalRotation));
+			Vector2d fPointL = getOrigin().copy().sub(new Vector2d(f, 0).rotate(totalRotation));
+
+			gc.strokeOval(fPointR.x - 5, fPointR.y - 5, 10, 10);
+			gc.strokeOval(fPointL.x - 5, fPointL.y - 5, 10, 10);
+		}
+	}
+
+	public void setPoints(double d, double r1, double r2) {
+		clearPoints();
+		int quarterResolution = getResolution() / 4;
+
 		double leftAngle = Math.acos(1 - d * d / (2 * r1 * r1)) / 2;
 		double rightAngle = Math.acos(1 - d * d / (2 * r2 * r2)) / 2;
 		double leftStep = leftAngle / quarterResolution;
@@ -28,7 +59,14 @@ public class OptimalConvexLens extends Lens {
 		for (int i = 0; i < quarterResolution * 2; i++) {
 			points.add(pos.copy().add(vec.rotate(rightStep)));
 		}
-		super.createBounds(); //Ta inte bort
-	}
 
+		points.add(points.get(0).copy()); // Close loop
+
+		super.restoreRotation();
+		super.createBounds();
+
+		this.r1 = r1;
+		this.r2 = r2;
+
+	}
 }
