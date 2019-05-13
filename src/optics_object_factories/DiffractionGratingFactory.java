cb18c4b290ced1c0a39682232a523761b0b37d18
Markus.Svedberg <markussv.747@gmail.com>
2019-05-13 15:02:09 +0200

    Nu med korrekt formel för utgångsvinkel! (Tror jag)

--- src/optics_object_factories/DiffractionGratingFactory.java
+++ src/optics_object_factories/DiffractionGratingFactory.java
@@ -13,7 +13,7 @@ public class DiffractionGratingFactory extends OpticsObjectFactory {
     public DiffractionGratingFactory() {
         addSlider("Slits per unit distance", 1000, 10000, 2000);
         addSlider("Height", 100, 400, 200);
-        addSlider("Number of maximums",1,12,4);
+        addSlider("Number of maximums",0,11,4);
     }
 
     @Override
@@ -24,7 +24,6 @@ public class DiffractionGratingFactory extends OpticsObjectFactory {
 
     @Override
     public void updateOpticsObject(OpticsObject object) {
-        System.out.println("NEW PARAMETERS");
         ((DiffractionGrating)object).updateGrating(GRATING_WIDTH, getParam("Height"),
                 getParam("Slits per unit distance"), getIntParam("Number of maximums"), LightSource.lightWaveDefault());
     }
--- src/optics_objects/lights/DiffractionGratingLightSource.java
+++ src/optics_objects/lights/DiffractionGratingLightSource.java
@@ -29,11 +29,16 @@ public class DiffractionGratingLightSource extends LightSource {
 		}
 
 		//Från https://en.wikipedia.org/wiki/Diffraction_grating
-		for(int m = 0; m < myDiffractionGrating.getNumMax(); m++){
+		for(int m = 0; m <= myDiffractionGrating.getNumMax(); m++){
 
 			//Stämmer detta ens???
-			double dy = Math.atan(Math.abs(Math.sin(latestAngleIn)) - m * myDiffractionGrating.getWaveLength() / myDiffractionGrating.getSlitsPerUnitDistance());
+			double angleOut = Math.asin(Math.sin(latestAngleIn)- m * myDiffractionGrating.getWaveLength() / myDiffractionGrating.getSlitsPerUnitDistance());
 
+			if(Double.isNaN(angleOut)){
+				continue;
+			}
+
+			double dy = Math.tan(angleOut);
 			super.addLightRay(new Vector2d(width/2 + 2, 0), new Vector2d(1, dy));
 			super.addLightRay(new Vector2d(width/2 + 2, 0), new Vector2d(1, -dy));
 		}
@@ -51,6 +56,6 @@ public class DiffractionGratingLightSource extends LightSource {
 		//convert the line to a normal:
 		lineSegment.rotate(cross*Math.PI / 2).normalize();
 		latestAngleIn = ray.angleTo(lineSegment);
-		System.out.println("Angle:" + Math.toDegrees(latestAngleIn));
+		//System.out.println("Angle:" + Math.toDegrees(latestAngleIn));
 	}
 }
