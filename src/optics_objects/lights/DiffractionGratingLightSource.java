901bdd922cb2d64af8c55843a22ddaba99eff3de
Marianne <Marianne@LAPTOP-9EMS9SMA>
2019-05-21 13:24:26 +0200

    Smaller changes making the program easier to read. Added the central maximum

--- src/optics_objects/lights/DiffractionGratingLightSource.java
+++ src/optics_objects/lights/DiffractionGratingLightSource.java
@@ -11,39 +11,47 @@ public class DiffractionGratingLightSource extends LightSource {
 	private double latestAngleIn = 0;
 	private int lastRayInSign = 1;
 
-	public DiffractionGratingLightSource(Vector2d origin, int width, boolean fixedPosition, DiffractionGrating diffractionGrating) {
+	public DiffractionGratingLightSource(Vector2d origin, int width, boolean fixedPosition,
+			DiffractionGrating diffractionGrating) {
 		super(origin, fixedPosition);
 		this.width = width;
 		this.myDiffractionGrating = diffractionGrating;
 		calculateLight();
 	}
 
-	public void calculateLight(){
+	public void calculateLight() {
 		super.clearLightRays();
 
-		if(myDiffractionGrating.getWaveLength() == -1){
+		if (myDiffractionGrating.getWaveLength() == -1) {
 			return;
 		}
 
 		boolean rightFacing = Math.toDegrees(totalRotation) % 360 > -90 && Math.toDegrees(totalRotation) % 360 < 90;
 
-		//Från https://en.wikipedia.org/wiki/Diffraction_grating
-		for(int m = 0; m <= myDiffractionGrating.getNumMax(); m++){
+		// adds the middle maximum
+		if ((lastRayInSign == 1 && rightFacing) || (lastRayInSign == -1) && !rightFacing) {
+			super.addLightRay(new Vector2d(width / 2 + 2, 0), new Vector2d(1, 0));
+		} else {
+			super.addLightRay(new Vector2d(-width / 2 - 2, 0), new Vector2d(-1, 0));
+		}
 
-			double angleOut = Math.asin(Math.sin(latestAngleIn)- m * myDiffractionGrating.getWaveLength() / myDiffractionGrating.getSlitSpacing());
+		// Från https://en.wikipedia.org/wiki/Diffraction_grating
+		for (int m = 0; m <= myDiffractionGrating.getNumMax(); m++) {
 
-			if(Double.isNaN(angleOut)){
+			double angleOut = Math.asin(Math.sin(latestAngleIn)
+					- m * myDiffractionGrating.getWaveLength() / myDiffractionGrating.getSlitSpacing());
+
+			if (Double.isNaN(angleOut)) {
 				continue;
 			}
 			double dy = Math.tan(angleOut);
 
-
-			if((lastRayInSign == 1 &&  rightFacing) || (lastRayInSign == -1) && !rightFacing) {
-				super.addLightRay(new Vector2d(width/2 + 2, 0), new Vector2d(1, dy));
-				super.addLightRay(new Vector2d(width/2 + 2, 0), new Vector2d(1, -dy));
-			}else {
-				super.addLightRay(new Vector2d(-width/2 - 2, 0), new Vector2d(-1, dy));
-				super.addLightRay(new Vector2d(-width/2 - 2, 0), new Vector2d(-1, -dy));
+			if ((lastRayInSign == 1 && rightFacing) || (lastRayInSign == -1) && !rightFacing) {
+				super.addLightRay(new Vector2d(width / 2 + 2, 0), new Vector2d(1, dy));
+				super.addLightRay(new Vector2d(width / 2 + 2, 0), new Vector2d(1, -dy));
+			} else {
+				super.addLightRay(new Vector2d(-width / 2 - 2, 0), new Vector2d(-1, dy));
+				super.addLightRay(new Vector2d(-width / 2 - 2, 0), new Vector2d(-1, -dy));
 			}
 		}
 
@@ -54,19 +62,19 @@ public class DiffractionGratingLightSource extends LightSource {
 	}
 
 	@Override
-	public void clearLightRays(){
+	public void clearLightRays() {
 		super.clearLightRays();
 	}
 
-	public void calculateInAngle(Vector2d ray){
+	public void calculateInAngle(Vector2d ray) {
 		Vector2d lineSegment = myDiffractionGrating.getSegment(0);
 		double cross = lineSegment.crossSign(ray);
-		//convert the line to a normal:
-		lineSegment.rotate(cross*Math.PI / 2).normalize();
+		// convert the line to a normal:
+		lineSegment.rotate(cross * Math.PI / 2).normalize();
 		latestAngleIn = ray.angleTo(lineSegment);
-		if(ray.x >= 0){
+		if (ray.x >= 0) {
 			lastRayInSign = 1;
-		}else {
+		} else {
 			lastRayInSign = -1;
 		}
 	}
