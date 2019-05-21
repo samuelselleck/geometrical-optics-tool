c07245f8aaca2f0b2c9b0a74d0206f1ad3e1f2a3
Samzelot <Samzelot@gmail.com>
2019-05-21 21:35:32 +0200

    finished new light interface

--- src/gui/optics_object_creators/GraphElement.java
+++ src/gui/optics_object_creators/GraphElement.java
@@ -1,29 +1,37 @@
 package gui.optics_object_creators;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import gui.Main;
 import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import model.optics_objects.LightSource;
-import util.Vector2d;
 
 public class GraphElement extends LineChart<Number, Number> {
 
 	public GraphElement() {
 		super(new NumberAxis(LightSource.lightWaveMin(), LightSource.lightWaveMax(), 100), new NumberAxis());
-		this.setPrefHeight(Main.HEIGHT/4);
+		this.setPrefHeight(Main.HEIGHT/5);
 		this.setLegendVisible(false);
 		this.getYAxis().setTickLabelsVisible(false);
 		this.getYAxis().setVisible(false);
+		this.setCreateSymbols(false);
+		this.setAnimated(false);
 		this.setPadding(new Insets(0, 15, 0, 0));
 	}
 	
-	public void setData(List<Vector2d> data) {
+	public void setData(List<Point2D> data) {
+		this.getData().clear();
 		XYChart.Series<Number, Number> series = new XYChart.Series<>();
+		series.getData().addAll(
+				data.stream()
+				.map(e -> new XYChart.Data<Number, Number>(e.getX(), e.getY()))
+				.collect(Collectors.toList()));
 		this.getData().add(series);
-		//TODO
+		series.getNode().setStyle("-fx-stroke: white;");
 	}
 }
--- src/gui/optics_object_creators/LightSourceCreator.java
+++ src/gui/optics_object_creators/LightSourceCreator.java
@@ -1,12 +1,47 @@
 package gui.optics_object_creators;
 
+import java.util.Arrays;
+
 import gui.OpticsView;
+import javafx.geometry.Point2D;
+import javafx.scene.control.Slider;
+import model.optics_objects.LightSource;
 
 public abstract class LightSourceCreator extends OpticsObjectCreator {
-
+	private GraphElement graph;
+	
 	public LightSourceCreator(OpticsView view) {
 		super(view);
-		this.addElement(new GraphElement());
+		graph = new GraphElement();
+		this.addElement(graph);
+		
+		Slider wavelength = addSlider("Wavelength", LightSource.lightWaveMin(), LightSource.lightWaveMax(), LightSource.lightWaveDefault());
+		Slider bandwidth = addSlider("Bandwidth", 0, LightSource.lightWaveMax() - LightSource.lightWaveMin(), 0);
+		wavelength.valueProperty().addListener(e -> {
+			double distToEdge = Math.min(
+					wavelength.getValue() - LightSource.lightWaveMin(),
+					LightSource.lightWaveMax() -  wavelength.getValue());
+			bandwidth.setValue(Math.min(bandwidth.getValue(), distToEdge*2));
+			setData(wavelength.getValue(), bandwidth.getValue());
+		});
+		bandwidth.valueProperty().addListener(e -> {
+			double dist = bandwidth.getValue()/2;
+			if(wavelength.getValue() - LightSource.lightWaveMin() < dist) {
+				wavelength.setValue(LightSource.lightWaveMin() + dist);
+			} else if (LightSource.lightWaveMax() - wavelength.getValue() < dist){
+				wavelength.setValue(LightSource.lightWaveMax() - dist);
+			}
+			setData(wavelength.getValue(), bandwidth.getValue());
+		});
+		
+		setData(wavelength.getValue(), bandwidth.getValue());
+	}
+	
+	private void setData(double wavelength, double bandwidth) {
+		graph.setData(Arrays.asList(
+				new Point2D(wavelength - bandwidth/2, 0),
+				new Point2D(wavelength, 1),
+				new Point2D(wavelength + bandwidth/2, 0)));
 	}
 
 }
